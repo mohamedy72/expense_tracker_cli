b2fa8e19c770232934c64ab24396bab84396c15f
Mohamed Yasser <mohamedwebdev94@gmail.com>
2026-05-24 14:22:08 +0300

    feat: list all expenses

--- features/list_expenses.py
+++ features/list_expenses.py
@@ -1,7 +1,15 @@
-def list_expenses(expenses):
+def list_expenses(expenses, category: None = None):
+
     for i, exp in enumerate(expenses, 1):
-        print("# " * 15 + "💴" + "Expense: " + str(i) + "💴" + " #" * 15)
-        print(("Category: " + " " * 5), exp["category"])
-        print(("Amount: " + " " * 5), exp["amount"])
-        print(("Description: " + " " * 5), exp["desc"])
-        print("#" * 50)
+        if category:
+            if exp["category"] == category:
+                print(("Category: " + " " * 5), exp["category"])
+                print(("Amount: " + " " * 5), exp["amount"])
+                print(("Description: " + " " * 5), exp["desc"])
+                print("*" * 50)
+        else:
+            print("# " * 15 + "💴" + "Expense: " + str(i) + "💴" + " #" * 15)
+            print(("Category: " + " " * 5), exp["category"])
+            print(("Amount: " + " " * 5), exp["amount"])
+            print(("Description: " + " " * 5), exp["desc"])
+            print("#" * 50)
